5eba979de77845d0ed0339d96c2a75917716cd32
Merzoug Imad-eddine <imadom568@gmail.com>
2021-05-27 16:38:28 +0200

    critical update

--- check_plesk_license.sh
+++ check_plesk_license.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-NAGIOS_RETURN_OK=0
-NAGIOS_RETURN_WARNING=1
-NAGIOS_RETURN_CRITICAL=2
-NAGIOS_RETURN_UNKNOWN=3
+NAGIOS_RETURN_OK = 0
+NAGIOS_RETURN_WARNING = 1
+NAGIOS_RETURN_CRITICAL = 2
+NAGIOS_RETURN_UNKNOWN = 3
 
 
-DATE_LIM=$(plesk bin keyinfo -l | grep lim_date | awk -F ':' '{print $2}')
-DATE=$(date +'%Y%m%d')
-DATE_LIM=$(date -d $DATE_LIM +"%Y%m%d") 
-DIFF=$(($DATE_LIM-$DATE))
+DATE_LIM = $(plesk bin keyinfo -l | grep lim_date | awk -F ':' '{print $2}')
+DATE = $(date +'%Y%m%d')
+DATE_LIM = $(date -d $DATE_LIM +"%Y%m%d") 
+DIFF = $(($DATE_LIM-$DATE))
 
 
-ALERT=$1
+ALERT = $1
 
 
-if [ $DIFF -le $ALERT ] ; then  echo "Hadek Houwa";  exit $NAGIOS_RETURN_CRITICAL  ; else echo "Mazel" ;exit $NAGIOS_RETURN_OK ; fi;
+if [ $DIFF -le $ALERT ] ; then  echo "Hadek Houwa" ;  exit $NAGIOS_RETURN_CRITICAL  ; else echo "Mazel" ;exit $NAGIOS_RETURN_OK ; fi ;
 
 
 
